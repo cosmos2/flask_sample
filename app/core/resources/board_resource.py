372b5d22d7263221a7a82253f20020b42342e0cb
cosmos2 <playnstop.s@gmail.com>
2021-08-18 16:51:34 +0900

    [ADD] board_resource

--- app/core/resources/board_resource.py
+++ app/core/resources/board_resource.py
@@ -1,35 +1,64 @@
-from flask import request
+from flask import request, jsonify
 from flask_restx import Resource
 from flask_restx import Namespace, fields
 
 from app.core.models.board import Board, board_schema, boards_schema
 
 api = Namespace('boards', description='board related operations')
-_user = api.model('board', {
-    'id': fields.Integer(),
+board_model = api.model('board', {
     'title': fields.String(description='board title')
 })
 
 
 @api.route('/')
 class BoardList(Resource):
-    @api.doc('Board list', response={200: 'OK'})
     def get(self):
+        """get board list"""
         boards = Board.query.all()
-        return {'data': boards_schema.dump(boards)}
+        return jsonify({'data': boards_schema.dump(boards)})
 
+    @api.response(201, 'Created')
+    @api.expect(board_model)
     def post(self):
+        """create new board"""
         payload = board_schema.load(request.get_json())
-        title = payload.get('title')
-        board = Board(title=title)
+        board = Board(**payload)
         board.save()
-        return {'data': board_schema.dump(board)}
+        return jsonify({'data': board_schema.dump(board)})
 
 
 @api.route('/<int:board_id>/')
 class BoardRetrieve(Resource):
-    @api.doc('Board retrieve', response={200: 'OK'})
     def get(self, board_id):
+        """get a board"""
         board = Board.query.filter_by(id=board_id).first()
-        return {'data': board_schema.dump(board)}
+        return jsonify({'data': board_schema.dump(board)})
+
+    @api.expect(board_model)
+    def patch(self, board_id):
+        """update board"""
+        board = Board.query.get(board_id)
+        payload = board_schema.load(request.get_json())
+
+        for key, value in payload.items():
+            setattr(board, key, value)
+        board.update()
+
+        return jsonify({'data': board_schema.dump(board)})
+
+    @api.response(204, 'No Content')
+    def delete(self, board_id):
+        board = Board.query.get(board_id)
+        board.delete()
+
+        return jsonify({'data': None})
+
+
+@api.route('<int:board_id>/articles/')
+class BoardWithArticle(Resource):
+    def get(self, board_id):
+        """get board with articles"""
+        if board := Board.query.get(board_id):
+            # TODO: some articles
+            pass
 
